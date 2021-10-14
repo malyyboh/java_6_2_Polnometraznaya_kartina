41f00242986dac223e5b13cf1672081057c2c230
Ksenx73 <hello@itcampe.com>
2021-10-14 16:28:24 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,12 @@
+package it.campe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int centimeters = 243;
+
+        int meters = 0; //напишите тут ваш код
+
+        System.out.println(meters);
+    }
+}
